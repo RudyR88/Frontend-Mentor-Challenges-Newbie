a519aac408aea5e1d8794483b3366b3513665374
RudyRivera <combustingkarma@gmail.com>
2022-12-04 23:17:52 -0600

    Project completed

---
+++ nft-preview-card-component/src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { data } from './js/data.js'
+import './scss/components/Card.css'
+
+function App() {
+
+  const {
+    cardImg,
+    cardImgAlt,
+    title,
+    desc,
+    iconImg,
+    unit,
+    timeRemaining,
+    creatorImg,
+    creator
+  } =  data
+
+  return (
+    <div className='card container'>
+      <a href="#product-img" className='img'>
+        <img className='product-img' src={cardImg} alt={cardImgAlt}/>
+        <div className='image-overlay flex-center'>
+          <img className='icon icon-view' src='../assets/images/icon-view.svg' alt='' aria-hidden='true'/>
+        </div>
+      </a>
+      <h2><a href="#product">{title}</a></h2>
+      <p>{desc}</p>
+      <div className='flex-between info'>
+        <div className='flex'>
+          <img className='icon' src={iconImg} alt='' aria-hidden='true'/>
+          <span className='unit-txt'>{unit}</span>
+        </div>
+        <div className='flex'>
+          <img className='icon icon-clock' src='../assets/images/icon-clock.svg' alt='' aria-hidden='true'/>
+          <span>{timeRemaining}</span>
+        </div>
+      </div>
+      <hr />
+      <div className='flex'>
+        <img className='avatar' src={creatorImg} alt='' aria-hidden='true'/>
+        <h3>Creation of <a href='#creator'>{creator}</a></h3>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ nft-preview-card-component/src/js/data.js
@@ -0,0 +1,12 @@
+export const data = {
+    cardImg: '../assets/images/image-equilibrium.jpg',
+    cardAlt: `clear 3d equilibrium cube standing on one point tilted to the right with red and blue lights 
+        shining on its top left and bottom faces with its reflection under it`,
+    title: 'Equilibrium #3429',
+    desc: 'Our Equilibrium collection promotes balance and calm.',
+    iconImg: '../assets/images/icon-ethereum.svg',
+    unit: '0.041 ETH',
+    timeRemaining: '3 days left',
+    creatorImg: '../assets/images/image-avatar.png',
+    creator: 'Jules Wyvern'
+}
